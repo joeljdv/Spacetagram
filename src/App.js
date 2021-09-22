4fd9266fd9d459f98ca8a7e640f01c0f18a21bb7
joeljdv <joeldiazjdv@gmail.com>
2021-09-21 20:13:21 -0400

    fecth images and set state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const[images, setImages] = useState({})
+
+  useEffect(() => {
+    fetch("https://api.nasa.gov/planetary/apod/?start_date=2021-08-16&api_key=tgFeXzfZOcVNfMfgzR5hIIFKVzIZ77bb8GPxgE55")
+    .then(r => r.json())
+    .then(data => {
+      setImages(data)
+      console.log("HE")
+    })
+    console.log("OK")
+  }, [])
+
   return (
     <div className="App">
-
+      <h1>Spacetagram</h1>
     </div>
   );
 }
