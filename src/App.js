e26b3848c3e08ef8b8aa273f1afc0d4a5cdf4239
joeljdv <joeldiazjdv@gmail.com>
2021-09-21 20:27:07 -0400

    iterate through images and create an image component for each

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import Image from './Image'
 
 function App() {
 
   const[images, setImages] = useState({})
 
+  // fetch images and save the data on state
   useEffect(() => {
     fetch("https://api.nasa.gov/planetary/apod/?start_date=2021-08-16&api_key=tgFeXzfZOcVNfMfgzR5hIIFKVzIZ77bb8GPxgE55")
     .then(r => r.json())
@@ -14,9 +16,14 @@ function App() {
     console.log("OK")
   }, [])
 
+  // create Image component for each image in state
+
+  const image = images.map( i => <Image key={i.id} image={i} />)
+
   return (
     <div className="App">
       <h1>Spacetagram</h1>
+      {image}
     </div>
   );
 }
--- src/Image.js
+++ src/Image.js
@@ -4,8 +4,11 @@ const Image = ({image}) => {
     return (
         <div>
             {image.title}
+            <br/>
             {image.date}
+            <br/>
             <img src={image.url} />
+            <hr/>
         </div>
     )
 }
